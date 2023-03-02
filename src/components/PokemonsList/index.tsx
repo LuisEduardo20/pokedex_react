c63edb777f43dbaa4c9a288f6ae2bb23776fff08
Luis Eduardo <luiseduardo20.dev@gmail.com>
2023-03-01 21:42:02 -0300

    refactor: pokemons list data

--- src/components/PokemonsList/index.tsx
+++ src/components/PokemonsList/index.tsx
@@ -2,24 +2,21 @@ import React from "react";
 import { Pagination } from "rsuite";
 
 import * as S from "./styles";
-import { GetAllPokemonsData } from "@/models/pokemons.service";
 import PokemonCard from "../PokemonCard";
+import { usePokemon } from "@/hooks/usePokemon";
 
 type Props = {
-  pokemonsData?: GetAllPokemonsData;
   page: number;
   handleChangePage: (page: number) => void;
 };
 
-const PokemonsList = ({
-  pokemonsData,
-  page,
-  handleChangePage,
-}: Props) => {
+const PokemonsList = ({ page, handleChangePage }: Props) => {
+  const { pokemonsList } = usePokemon();
+
   return (
     <S.Container>
       <S.ListContainer>
-        {pokemonsData?.results?.map((pokemonInfo, index) => (
+        {pokemonsList?.results?.map((pokemonInfo, index) => (
           <PokemonCard
             pokemonData={pokemonInfo}
             key={`pokemon-${index}`}
@@ -35,7 +32,7 @@ const PokemonsList = ({
         ellipsis
         maxButtons={10}
         size='lg'
-        total={pokemonsData?.count || 0}
+        total={pokemonsList?.count || 0}
         limit={20}
         activePage={page}
         onChangePage={(newPage) => handleChangePage(newPage)}
