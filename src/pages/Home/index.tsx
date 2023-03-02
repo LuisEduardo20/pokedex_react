299c8ee2a34d8f444a6577f479ae390394e4f69e
Luis Eduardo <luiseduardo20.dev@gmail.com>
2023-03-01 21:42:36 -0300

    refactor: set pokemons list

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,38 +4,33 @@ import { useQuery } from "react-query";
 import * as S from "./styles";
 import { PokemonServices } from "@/services/pokemon.services";
 import { Header, PokemonsList } from "@/components";
+import { usePokemon } from "@/hooks/usePokemon";
 
 type Props = {};
 
 const Home = (props: Props) => {
-  const [pokemonName, setPokemonName] = useState<string>("");
+  const { setPokemonsList } = usePokemon();
+
   const [page, setPage] = useState<number>(1);
 
-  const { data, isLoading, error } = useQuery(
+  const { isLoading, error } = useQuery(
     ["pokemon", page],
-    async () =>
-      await PokemonServices.getPokemonsWithPagination((page - 1) * 20)
+    async () => {
+      const pageOffset = (page - 1) * 20;
+
+      const { data } =
+        await PokemonServices.getPokemonsWithPagination(pageOffset);
+
+      setPokemonsList(data);
+    }
   );
 
-  const handleChangePage = async (page: number) => {
-    try {
-      setPage(page);
-    } catch (error) {
-      console.log("error:", error);
-    }
-  };
-
-  // console.log("isLoading:", isLoading);
-  // console.log("data:", data?.data);
+  const handleChangePage = async (page: number) => setPage(page);
 
   return (
     <S.Container>
       <Header />
-      <PokemonsList
-        page={page}
-        pokemonsData={data?.data}
-        handleChangePage={handleChangePage}
-      />
+      <PokemonsList page={page} handleChangePage={handleChangePage} />
     </S.Container>
   );
 };
